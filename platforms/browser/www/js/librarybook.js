0450ec0cc9eedcd33724b5512f33f3a85c9230ad
Alibabas <danny.nemtanu@gmail.com>
2017-04-19 20:46:18 +0100

    Stiled the library page

--- platforms/browser/www/js/librarybook.js
+++ platforms/browser/www/js/librarybook.js
@@ -21,31 +21,30 @@ function openCity(evt, cityName) {
 }
 function loadingBook(){
   //first line
-    $.ajax({
-      url: 'http://localhost:3002/api/libraries'
-    }).then(function (data) {
-      // console.log(data);
-      for(let i=0;i<data.length;i++){
-        if(data[i].category==="Social Sciences"){
-         $('.content-block #social-science-category').append(
-          '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img id="social" src='+"../img/social.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-            '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
-         );
+  $.ajax({
+    url: 'http://localhost:3002/api/libraries'
+  }).then(function (data) {
+    // console.log(data);
+    for(let i=0;i<data.length;i++){
+      if(data[i].category==="Social Sciences"){
+       $('.content-block #social-science-category').append(
+         '<div class="book-content">'+
+           '<div id="bookitem">'+
+             '<div id="book-img"><img id="social" src='+'"../img/social.jpg"'+'></img></div>'+
+             '<div id="bookdesc">'+
+               '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+               '<p>'+data[i].description+'</p>'+
+             '</div>'+
+           '</div>'+
+           '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+         '</div>'
+       );
+     }
+   }
+ });
 
-        }
-      }
-    });
 
-  //second line
+//   //second line
 
    $.ajax({
       url: 'http://localhost:3002/api/libraries'
@@ -54,22 +53,22 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="Arts and recreation"){
          $('.content-block #arts-and-recreation-category').append(
-          '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/art.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-            '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/art.jpg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
       }
     });
+
     //third line
     $.ajax({
       url: 'http://localhost:3002/api/libraries'
@@ -78,19 +77,17 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="Religion"){
          $('.content-block #religion-category').append(
-            '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/Religion.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-            '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/Religion.jpg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
-
         }
       }
     });
@@ -102,17 +99,16 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="Literature"){
          $('.content-block #literature-category').append(
-          '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/Literature.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-            '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/Literature.jpg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
@@ -126,17 +122,16 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="Science"){
          $('.content-block #science-category').append(
-           '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/Science.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-            '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/Science.jpg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
@@ -152,17 +147,16 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="information and general works"){
          $('.content-block #information-and-general-works-category').append(
-           '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/information.jpeg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-            '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/information.jpeg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
@@ -176,17 +170,16 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="Technology and applied science"){
          $('.content-block #technology-and-applied-science-category').append(
-           '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/appliedscience.png"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-           '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/appliedscience.png"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
@@ -201,17 +194,16 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="Philosophy and psychology"){
          $('.content-block #philosophy-and-psychology-category').append(
-           '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/Philosophy.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-           '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/philosophy.jpg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
@@ -225,17 +217,16 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="Computer science"){
          $('.content-block #computer-science-category').append(
-           '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/computer.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-            '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/computer.jpg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
@@ -249,17 +240,16 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="Language"){
          $('.content-block #language-category').append(
-           '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/Language.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-            '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/Language.jpg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
@@ -274,21 +264,20 @@ function loadingBook(){
       for(let i=0;i<data.length;i++){
         if(data[i].category==="History and geography"){
          $('.content-block #history-and-geography-category').append(
-           '<table>'+
-          '<div id="book-block' + i + '">' +
-            '<tr>'+'<td>'+'<img src='+"../img/History.jpg"+'></img>'+'</td>'+
-            '<td>'+'<h5>Book title : '+data[i].title+'</h5>'+
-            '<h5>Book ID : '+data[i].bookID+'</h5>'+
-            '<h5>Book author : '+data[i].author+'</h5>'+
-            '<h5>Book description : '+data[i].description+'</td>'+'</tr>'+
-           '<tr>'+'<td>'+
-            '<button id="myBtn" onclick="myFunction()">Reserve</button>'+
-            '</td>'+'</tr>'+
-          '</div>'+'</table>'
+           '<div class="book-content">'+
+             '<div id="bookitem">'+
+               '<div id="book-img"><img src="../img/History.jpg"></img></div>'+
+               '<div id="bookdesc">'+
+                 '<h1>'+(data[i].title).toUpperCase()+'</h1>'+
+                 '<p>'+data[i].description+'</p>'+
+               '</div>'+
+             '</div>'+
+             '<button type="button" id="myBtn" onclick="myFunction()" name="button">Reserve Book</button>'+
+           '</div>'
          );
 
         }
       }
     });
-
+//
 }
